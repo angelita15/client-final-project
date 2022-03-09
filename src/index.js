dbc7ae63e7e0666a13fa6355433a71e4ec55f3a1
angelita15 <95506994+angelita15@users.noreply.github.com>
2022-03-09 15:36:10 +0100

    browserRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
 import { AuthProviderWrapper } from './context/auth.contex'
 
