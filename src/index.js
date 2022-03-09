98ae88edb419b4ba8f35eab30e842561f21858f1
angelita15 <95506994+angelita15@users.noreply.github.com>
2022-03-09 15:40:11 +0100

    HashRouter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router} from 'react-router-dom'
 import App from './App';
-import { AuthProviderWrapper } from './context/auth.contex'
+import {AuthProviderWrapper} from './context/auth.contex'
 
 ReactDOM.render(
   <Router>
-    <AuthProviderWrapper>
-      <App />
+    <AuthProviderWrapper> 
+    <App />
     </AuthProviderWrapper>
   </Router>,
   document.getElementById('root')
